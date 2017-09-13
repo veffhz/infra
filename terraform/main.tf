24a9c6b0bcfa518909bd3644c815ebd1c9bd428b
Vladimir Danilyuk <veffhz@gmail.com>
2017-09-13 23:24:45 +0300

    Add vars files, deploy.sh.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,18 @@
 provider "google" {
-  project ="week-3-178420"
-  region = "europe-west1"
+  project = "${var.project}"
+  region  = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = "europe-west1-b"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
 
   # определение загрузочного диска
   boot_disk {
     initialize_params {
-      image = "reddit-base-1504639663"
+      image = "${var.disk_image}"
     }
   }
 
@@ -19,7 +20,43 @@ resource "google_compute_instance" "app" {
   network_interface {
     # сеть, к которой присоединить данный интерфейс
     network = "default"
+
     # использовать ephemeral IP для доступа из Интернет
     access_config {}
   }
+
+  connection {
+    type        = "ssh"
+    user        = "${var.user_name}"
+    agent       = false
+    private_key = "${file("~/.ssh/appuser")}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с тегом ...
+  target_tags = ["reddit-app"]
 }
