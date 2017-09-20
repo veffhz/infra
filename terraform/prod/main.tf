07a335343e73fccf225bdd7131eaeb4c52981c48
Vladimir Danilyuk <veffhz@gmail.com>
2017-09-20 23:56:23 +0300

    Add stage, prod env.

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -4,20 +4,20 @@ provider "google" {
 }
 
 module "app" {
-  source          = "modules/app"
+  source          = "../modules/app"
   public_key_path = "${var.public_key_path}"
   app_disk_image  = "${var.app_disk_image}"
   user_name       = "${var.user_name}"
 }
 
 module "db" {
-  source          = "modules/db"
+  source          = "../modules/db"
   public_key_path = "${var.public_key_path}"
   db_disk_image   = "${var.db_disk_image}"
   user_name       = "${var.user_name}"
 }
 
 module "vpc" {
-  source        = "modules/vpc"
-  source_ranges = ["0.0.0.0/0"]
+  source        = "../modules/vpc"
+  source_ranges = ["195.91.236.241/32"]
 }
